e369bd22dfb884a9534500a78e7e79952da35778
Garrett Smith <garrettsmith78@gmail.com>
2026-03-21 18:57:26 +0100

    WIP: Getting plot sending working

--- SimulateAndPlot/src/SimulateAndPlot.jl
+++ SimulateAndPlot/src/SimulateAndPlot.jl
@@ -1,5 +1,6 @@
 module SimulateAndPlot
 
+using CairoMakie
 using Distributions
 using Statistics
 
@@ -20,11 +21,35 @@ function calculate_mean_and_ci_half_width(data)
     return vec(means), 1.96 .* vec(std_error_of_mean)
 end
 
+function indices_where_zero_is_excluded(means, ci_half_widths)
+    (means .- ci_half_widths) .> 0 .|| (means .+ ci_half_widths) .< 0
+end
+
 function number_that_dont_include_zero(means, ci_half_widths)
-    count((means .- ci_half_widths) .> 0 .|| (means .+ ci_half_widths) .< 0)
+    count(indices_where_zero_is_excluded(means, ci_half_widths))
+end
+
+function fig_to_svg(fig)
+    buf = IOBuffer()
+    CairoMakie.save(buf, fig)
+    return String(take!(buf))
+end
+
+function make_plot(means, ci_half_widths)
+    n_values = length(means)
+    color_to_use = fill(:green, n_values)
+    color_to_use[indices_where_zero_is_excluded(means, ci_half_widths)] .= :red
+
+    CairoMakie.activate!(type="svg")
+    fig = Figure()
+    ax = Axis(fig[1, 1])
+    errorbars!(ax, 1:n_values, means, ci_half_widths; color=color_to_use)
+    scatter!(ax, 1:n_values, means; color=color_to_use)
+    return fig_to_svg(fig)
 end
 
 export ExperimentConfig
 export generate_data, calculate_mean_and_ci_half_width, number_that_dont_include_zero
+export make_plot
 
 end
--- SimulateAndPlot/src/main.jl
+++ SimulateAndPlot/src/main.jl
@@ -6,7 +6,9 @@ using SimulateAndPlot
 @post "/" function (request::HTTP.Request)
     exp_info = json(request, ExperimentConfig)
     means, ci_half_widths = calculate_mean_and_ci_half_width(generate_data(exp_info))
-    return number_that_dont_include_zero(means, ci_half_widths)
+    # return number_that_dont_include_zero(means, ci_half_widths),
+    fig_svg = make_plot(means, ci_half_widths)
+    return HTTP.Response(200; headers=["Content-Type" => "image/svg+xml"], body=fig_svg)
 end
 
 @get "/healthz" function ()
