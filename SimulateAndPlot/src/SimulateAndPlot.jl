b7c39a25d80b5e67db0232a1b401f490983a2152
Garrett Smith <garrettsmith78@gmail.com>
2026-03-21 13:09:56 +0100

    Add health check endpoint, correct calculation of CI bounds

--- SimulateAndPlot/src/SimulateAndPlot.jl
+++ SimulateAndPlot/src/SimulateAndPlot.jl
@@ -4,31 +4,35 @@ using HTTP
 using JSON3
 using Oxygen
 
-struct ExperimentInfo
+struct ExperimentConfig
     n_experiments::Int
     n_samples_per_experiment::Int
 end
 
 
-function generate_data(exp_info::ExperimentInfo)
+function generate_data(exp_info::ExperimentConfig)
     rand(Normal(), (exp_info.n_experiments, exp_info.n_samples_per_experiment))
 end
 
 function calculate_mean_and_ci_half_width(data)
     means = mean(data; dims=2)
     vars = var(data; dims=2)
-    std_error_of_mean = sqrt.(vars) ./ size(data)[2]
-    return vec(means), 2 .* vec(std_error_of_mean)
+    std_error_of_mean = sqrt.(vars ./ size(data)[2])
+    return vec(means), 1.96 .* vec(std_error_of_mean)
 end
 
 function number_that_dont_include_zero(means, ci_half_widths)
-    count((means.-ci_half_widths) .> 0 .|| (means.+ci_half_widths) .< 0)
+    count((means .- ci_half_widths) .> 0 .|| (means .+ ci_half_widths) .< 0)
 end
 
 @post "/" function (request::HTTP.Request)
-    exp_info = json(request, ExperimentInfo)
+    exp_info = json(request, ExperimentConfig)
     means, ci_half_widths = calculate_mean_and_ci_half_width(generate_data(exp_info))
     return number_that_dont_include_zero(means, ci_half_widths)
+end
+
+@get "/healthz" function ()
+    HTTP.Response(200)
 end
 
 serve()
