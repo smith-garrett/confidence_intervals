51329fbeec06128b6f5cf9907fdddaf6c020e16a
Garrett Smith <garrettsmith78@gmail.com>
2026-03-22 09:55:17 +0100

    Get figure formatted.

--- SimulateAndPlot/src/SimulateAndPlot.jl
+++ SimulateAndPlot/src/SimulateAndPlot.jl
@@ -36,15 +36,21 @@ function fig_to_svg(fig)
 end
 
 function make_plot(means, ci_half_widths)
+    # TODO: Sort the means so that the extreme values are more easily found
     n_values = length(means)
-    color_to_use = fill(:green, n_values)
+    color_to_use = fill(:teal, n_values)
     color_to_use[indices_where_zero_is_excluded(means, ci_half_widths)] .= :red
+    marker_to_use = fill(:circle, n_values)
+    marker_to_use[indices_where_zero_is_excluded(means, ci_half_widths)] .= :xcross
 
     CairoMakie.activate!(type="svg")
     fig = Figure()
-    ax = Axis(fig[1, 1])
+    ax = Axis(fig[1, 1], xlabel="Experiment", ylabel="Sample mean", title="Distribution of sample means with 95% CIs")
+    hidexdecorations!(ax, label=false)
+    hideydecorations!(ax, label=false, ticks=false, ticklabels=false)
+    hlines!(ax, [0.0], color=:black)
     errorbars!(ax, 1:n_values, means, ci_half_widths; color=color_to_use)
-    scatter!(ax, 1:n_values, means; color=color_to_use)
+    scatter!(ax, 1:n_values, means; color=color_to_use, marker=marker_to_use)
     return fig_to_svg(fig)
 end
 
